7f58becbc937a3705f9a43bf2ea7e5fb33a9e836
Ingrid-Chou <113267488+Ingrid-Chou@users.noreply.github.com>
2022-09-24 23:25:50 -0700

    Add files via upload

---
+++ Part-1/Week3/Increment-Decrement/example.js
@@ -0,0 +1,23 @@
+//Incrementing
+var x = 0;
+
+x = x + 1;
+console.log(x);
+
+x += 1;
+console.log(x);
+
+x++;
+console.log(x);
+
+//Decrementing
+var num = 10;
+
+num = num - 2;
+console.log(num);
+
+num -= 3;
+console.log(num);
+
+num--;
+console.log(num);
