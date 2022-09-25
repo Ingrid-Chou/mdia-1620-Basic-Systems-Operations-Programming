a136f0b5aa5607c90cc98c777db8d06d2500dcfd
Ingrid-Chou <ichou8@my.bcit.ca>
2022-09-24 23:08:56 -0700

    new line

---
+++ Part-1/Week3/test.js
@@ -0,0 +1,20 @@
+//This is a single line comment
+
+/**
+ *  This is a description
+ * hi there
+ *  again
+ */
+
+var x = myFunction(2,3);
+/**
+ * @desc This calculates the area of a floor
+ * @param {*} numOne is the width
+ * @param {*} numTwo is the height
+ * @returns returns the area of the floor
+ */
+function myFunction(numOne, numTwo){
+    return numOne * numTwo;
+}
+
+// adding some stuff
